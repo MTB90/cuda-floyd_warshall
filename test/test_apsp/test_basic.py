2ef623278d16fbb0a70c36ab25ce0d5b05938cf9
Mateusz Bojanowski <bojanowski.mateusz@gmail.com>
2018-01-19 01:48:25 +0100

    Added simple template for tests in Python for APSP

---
+++ test/test_apsp/test_basic.py
@@ -0,0 +1,12 @@
+"""
+Simple tests for APSP
+package unittest is used because it helps with test management
+and also provided formatted results for tests.
+"""
+
+import unittest
+
+
+class TestBasic(unittest.TestCase):
+    def test_GIVEN_WHEN_THEN(self):
+        pass
